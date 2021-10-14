e719da715c7b67fd9b092251226063deafca4817
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-10-14 14:53:56 +0300

    Create goldbachs-conjecture

---
+++ goldbachs-conjecture/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static boolean isPrime(int number) {
+        if (number <= 1)
+            return false;
+
+        for (int divisor = 2; divisor < number / 2; divisor++)
+            if (number % divisor == 0)
+                return false;
+
+        return true;
+    }
+
+    public static boolean checkGoldbachsConjecture(int number) {
+        if ((number > 2) && (number % 2 == 0)) {
+            var arrayOfPrimeNumbers = new int[100];
+            int numberOfPrimeNumbers = 0;
+
+            for (int i = 0; i < number; i++)
+                if (isPrime(i)) {
+                    arrayOfPrimeNumbers[numberOfPrimeNumbers] = i;
+
+                    numberOfPrimeNumbers += 1;
+                }
+
+            for (int i = 0; i < numberOfPrimeNumbers; i++)
+                for (int j = 0; j < numberOfPrimeNumbers; j++) {
+                    int sum = arrayOfPrimeNumbers[i] + arrayOfPrimeNumbers[j];
+
+                    if (sum == number)
+                        return true;
+                }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    public static void testFunction() {
+        System.out.println(checkGoldbachsConjecture(8));
+        System.out.println(checkGoldbachsConjecture(7));
+    }
+
+    public static void main(String[] args) {
+        testFunction();
+    }
+}
